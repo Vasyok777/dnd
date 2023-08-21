ac919b78f0ba79d1482534c651a2760a08bb8eb8
Vasyl Holub <vasyok.robota@gmail.com>
2023-08-21 15:27:32 +0300

    final project

--- src/store/slice/todo/asyncActions.js
+++ src/store/slice/todo/asyncActions.js
@@ -1,11 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import axios from 'axios'
+import { instance } from '../../../config/axios'
 
 export const fetchTodo = createAsyncThunk('todo/fetchTodoStatus', async () => {
 	try {
-		const response = await axios.get(
-			'https://jsonplaceholder.typicode.com/todos'
-		)
+		const response = await instance.get('')
 		return response.data
 	} catch (error) {
 		throw Error('Failed to fetch data from the server')
